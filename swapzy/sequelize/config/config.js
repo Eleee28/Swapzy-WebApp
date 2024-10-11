6f540f0199887f1315581250c5938d7e3154f609
Eleee28 <elena.juarrosgonzalez@gmail.com>
2024-10-12 00:54:36 +0100

    sequelize folder

---
+++ swapzy/sequelize/config/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_URL,
+    dialect: 'postgres',
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnathorized: false, 
+    //   },
+    // },
+  },
+  test: {
+    username: root,
+    password: null,
+    database: database_test,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: root,
+    password: null,
+    database: database_production,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  }
+}
