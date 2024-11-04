e85255f0d73cf9481aa1a52ddb40cc179131b715
Eleee28 <elena.juarrosgonzalez@gmail.com>
2024-11-04 13:16:43 +0000

    crud control

---
+++ swapzy/controller/userController.js
@@ -0,0 +1,92 @@
+const bcrypt = require('bcryptjs')
+const User = require('../sequelize/models/users');
+
+// Controller method to get all users
+exports.getAllUsers = async function (req, res) {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error", error: err.message });
+    }
+};
+
+// Controller method to get a user by id
+exports.getUserById = async function (req, res) {
+    const id = req.params.id;
+
+    try {
+        const user = await User.findByPk(id);
+        if (!user)
+            res.status(404).send("User not found");
+        else
+            res.json(user);
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error", error: err.message });
+    }
+};
+
+// Controller method to create a new user
+exports.createUser = async function (req, res) {
+    const { username, email, password, location, image_url } = req.body;
+    hashedPassword = await bcrypt.hash(password, 8);
+
+    try {
+        const newUser = await User.create({
+            username,
+            email,
+            hashedPassword,
+            location,
+            image_url,
+        });
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error", error: err.message });
+    }
+};
+
+// Controller method to update a user by id
+exports.updateUser = async function (req, res) {
+    const id = req.params.id;
+    const { username, email, password, location, image_url } = req.body;
+
+    try {
+        const user = await User.findByPk(id);
+        if (user) {
+            if (username && username !== user.username)
+                user.username = username;
+            if (email && email !== user.email)
+                user.email = email;
+            if (password)
+                user.password = await bcrypt.hash(password, 8);
+            if (location && location !== user.location)
+                user.location = location;
+            if (image_url && image_url !== user.image_url)
+                user.image_url = image_url;
+
+            await user.save();
+            res.json(user);
+        } else {
+            res.status(404).send("User not found");
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error", error: err.message });
+    }
+}
+
+// Controller method to delete a todo by id
+exports.deleteUser = async function (req, res) {
+    const id = req.params.id;
+
+    try {
+        const user = await User.findByPk(id);
+        if (user) {
+            await user.destroy();
+            res.json(user);
+        } else {
+            res.status(404).send("User not found");
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error", error: err.message });
+    }
+};
