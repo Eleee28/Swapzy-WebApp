28af1c4c94860e9aaf4f9b455cef5b330712e2db
Eleee28 <elena.juarrosgonzalez@gmail.com>
2024-11-25 21:04:29 +0000

    nav bar functionality

---
+++ swapzy/view/javascript/nav-bar.js
@@ -0,0 +1,38 @@
+async function navButtonHandler() {
+    const favButton = document.getElementById("fav-button");
+    const profileButton = document.getElementById("profile-button");
+    const sellButton = document.getElementById("sell-button");
+
+    const response = await fetch('/api/check-login');
+    const data = await response.json();
+
+    favButton.addEventListener('click', () => {
+        console.log('Fav button clicked');
+    });
+
+    profileButton.addEventListener('click', () => {
+        try {
+            if (data.isLoggedIn) {
+                window.location.href = 'settings.html';
+            } else {
+                window.location.href = 'login.html';
+            }
+        } catch (err) {
+            console.error('Error: ', err);
+        }
+    });
+
+    sellButton.addEventListener('click', () => {
+        try {
+            if (data.isLoggedIn) {
+                window.location.href = 'upload_product.html';
+            } else {
+                alert('You must be logged in to sell a product');
+            }
+        } catch (err) {
+            console.error('Error: ', err);
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', navButtonHandler);
